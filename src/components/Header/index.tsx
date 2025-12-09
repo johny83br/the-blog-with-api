e1fa6b15925e4f2b5d49b295e73acb7f29784b58
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-12-09 17:53:08 -0300

    Ajustes no cabeçalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,6 @@ import clsx from 'clsx';
 import Link from 'next/link';
 
 export function Header() {
-  console.log('HEADER');
-
   return (
     <header>
       <h1
@@ -14,7 +12,7 @@ export function Header() {
           'lg:text-7xl/normal lg:py-12',
         )}
       >
-        <Link href='#'>The Blog</Link>
+        <Link href='/'>The Blog</Link>
       </h1>
     </header>
   );
