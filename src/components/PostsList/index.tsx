1deaf1d4cb11d86f87808180f5ab490427668ef6
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-12-09 17:40:10 -0300

    Criado o footer e feito mais alguns ajustes finos

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -7,7 +7,7 @@ export async function PostsList() {
   const posts = await findAllPublicPosts();
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8'>
       {posts.slice(1).map(post => {
         const link = `/post/${post.slug}`;
 
