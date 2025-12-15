3a823351a43b6adf05acb18b811bf811b3857ace
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-12-15 17:52:16 -0300

    Ajustes no carregamento

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,21 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center',
-      )}
-    >
-      <div>
-        <h1 className='text-7xl/tight mb-4 font-extrabold'>Erro 404</h1>
-        <p>A página que você está tentando acessar não existe neste site.</p>
+    <>
+      <title>Página não encontrada | The Blog</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100',
+          'mb-16 p-8 rounded-xl',
+          'flex items-center justify-center',
+          'text-center',
+        )}
+      >
+        <div>
+          <h1 className='text-7xl/tight mb-4 font-extrabold'>Erro 404</h1>
+          <p>A página que você está tentando acessar não existe neste site.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
