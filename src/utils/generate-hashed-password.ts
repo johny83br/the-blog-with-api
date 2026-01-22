483ce6ebdb34c1305b1d133f99821d847add99d7
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-01-22 17:47:20 -0300

    Ajustes finais

--- src/utils/generate-hashed-password.ts
+++ src/utils/generate-hashed-password.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/lib/login/manage-login';
 
 (async () => {
-  const password = '$RNj!\;hU7_9RQ1_-ilFM}8.Seg#B0Z'; // NÃO ESQUECER DE APAGAR SENHA DAQUI
+  const password = ''; // NÃO ESQUECER DE APAGAR SENHA DAQUI
   const hashedPassword = await hashPassword(password);
   console.log(hashedPassword);
 })();
