6085f528c86343c210a7993ab5ad793ef24c4b56
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-01-27 01:34:20 -0300

    Colocado o subtitulo no formulario de atualizacao do usuario

--- src/app/admin/user/page.tsx
+++ src/app/admin/user/page.tsx
@@ -1,4 +1,5 @@
 import { UpdateUser } from '@/components/Admin/UpdateUser';
+import { Subtitle } from '@/components/Layouts/Subtitle';
 import { SpinLoader } from '@/components/Others/SpinLoader';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
@@ -6,12 +7,13 @@ import { Suspense } from 'react';
 export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
-  title: 'User Admin',
+  title: 'Seus Dados',
 };
 
 export default async function AdminUserPage() {
   return (
     <Suspense fallback={<SpinLoader className='mb-16' />}>
+      <Subtitle>Seus Dados</Subtitle>
       <UpdateUser />
     </Suspense>
   );
